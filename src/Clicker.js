2c8ff4716d3dd3a474aa9edbfdf9746fe7913c70
tangweikun <819573105@qq.com>
2019-11-15 10:34:06 +0800

    :lipstick: update ui of clicker

--- src/Clicker.js
+++ src/Clicker.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import "./Clicker.css";
+import DECREASE_ICON from "./assets/decrease.svg";
+import PLUS_ICON from "./assets/plus.svg";
+import REFRESH_ICON from "./assets/refresh.svg";
 
 export function Clicker() {
   const [count, setCount] = useState(0);
@@ -21,13 +24,13 @@ export function Clicker() {
       <div className="clicker-display">{count}</div>
       <div className="clicker-button-panel">
         <div className="btn-green" onClick={handleAdd}>
-          +
+          <img src={PLUS_ICON} alt="plus" />
         </div>
         <div className="btn-orange" onClick={handleRefresh}>
-          refresh
+          <img src={REFRESH_ICON} alt="refresh" />
         </div>
         <div className="btn-red" onClick={handleDecrease}>
-          -
+          <img src={DECREASE_ICON} alt="decrease" />
         </div>
       </div>
     </div>
